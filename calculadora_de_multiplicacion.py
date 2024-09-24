265636768cf6fdf6d35bf8f3f10b61adae1be833
DylanAaronElicserioMartinez <a.23308051281179@cbtis128.edu.mx>
2024-09-23 20:27:07 -0600

    Add files via upload

---
+++ calculadora_de_multiplicacion.py
@@ -0,0 +1,20 @@
+print(" ")
+print("Dylan Aaron Elicserio Martínez esta es mi practica:")
+print(" ")
+#Solicitar al usuario que ingrese el primer numero
+numero1=input("ingresa el primer numero:")
+#Conversion del primer numero caracter entero
+num1=int(numero1)
+#Solicitar al usuario que ingrese el sugundo numero
+numero2=input("ingresa el segundo numero:")
+#Conversion del segundo numero a caracter entero
+num2=int(numero2)
+#simplificando la linea de comando
+numero3=int(input("ingresa el tercer numero:"))
+#Conversion del tercer numero a caracter entero
+num3=int(numero3)
+#multiplicar el valor de de los 3 numeros
+multiplicar=num1*num2*num3
+#Muestra el resultado del calculo de la multiplicacion de los 3 numeros
+resultado=print("El Resultado es:")
+print(multiplicar)
